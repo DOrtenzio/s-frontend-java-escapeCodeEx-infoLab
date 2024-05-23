f7dbbd1898c06524455ea16cd1f701c4d9df68dd
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-05-23 11:37:34 +0200

    Update Asterischi.java

--- Asterischi.java
+++ Asterischi.java
@@ -1,6 +1,20 @@
-public class Main {
-    public static void Main(String [] args){
-
+public class Asterischi {
+    public static void main(String[] args){
+        quadrato(7);
+    }
+    public static void quadrato(int dim) {
+        for(int i=0;i<dim;i++){ //RIghe
+            for(int p=0;p<dim;p++){ //Colonne
+                if (i==0 || i==dim-1) {
+                    System.out.print("*");
+                }else if(p==0 || p==dim-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print();
+                }
+            }
+            System.out.println();
+        }
     }
     public static String posizione(int x, int y) {
         return "\033["+ y +";2"+ x +"H";
